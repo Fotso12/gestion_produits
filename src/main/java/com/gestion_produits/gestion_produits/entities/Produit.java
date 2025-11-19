d12b23090d3cd57ff5b3a95ed0e0de7c6f07be82
Fotso12 <tamofotso90@gmail.com>
2025-11-19 17:03:30 +0100

    création de l'entité produit et configuration du application.properties pour la base de donnée

--- src/main/java/com/gestion_produits/gestion_produits/entities/Produit.java
+++ src/main/java/com/gestion_produits/gestion_produits/entities/Produit.java
@@ -1,4 +1,30 @@
 package com.gestion_produits.gestion_produits.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Produits")
+@Getter
+@Setter
 public class Produit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Basic(optional = false)
+    @Column(unique = true)
+    private String nomProduit;
+
+    @Basic(optional = false)
+    private String description;
+
+    @Basic(optional = false)
+    private String reference;
+
+    @Basic(optional = false)
+    private String type;
+
 }
